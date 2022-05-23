665d3b358e6e4026c4bb173bf5f81f3fbca2e5b4
Jing <wongje11@gmail.com>
2022-05-24 02:24:09 +0800

    initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,13 +73,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+      'default': {
+          'ENGINE': 'djongo',
+          'NAME': 'backend',
+      }
+  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
