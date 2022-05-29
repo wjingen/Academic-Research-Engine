7705fe5c9633dbed7dbfc326e2dcea613a00c3d7
Yi Kuang Loh <yikuang5@gmail.com>
2022-05-29 12:43:24 +0800

    edit(frontend): package dependencies

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import App from './App'
-import {createRoot} from "react-dom/client"
-import {StrictMode} from 'react';
+import { createRoot } from "react-dom/client"
+import { StrictMode } from 'react';
 
 import store from './app/store'
 import { Provider } from 'react-redux'
