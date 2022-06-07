422979190ae35593501a1df02df1f08a92750225
Jing En <wongje11@gmail.com>
2022-06-07 23:09:44 +0800

    Feat[FE]: Add functional backend for main page
    Includes:
    - ResearchArchiveSerializer created in serializers.py
    - Python functions in utils.py
    - Views to accept HTTP Requests
    - urls.py to direct request to appropriate view

--- backend/research/models.py
+++ backend/research/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ResearchArchive(models.Model):
+    Title = models.CharField(max_length=500, default=None)
+    TitleID = models.CharField(max_length=150, default=None)
+    Link = models.CharField(max_length=150, default=None)
+    PubYear = models.IntegerField(default=None)
+    DateAdded = models.CharField(max_length=150, default=None)
+    Category = models.CharField(max_length=150, default=None)
+    UserID = models.CharField(max_length=150, default=None)
+
+    Abstract = models.TextField(null=True, default=None)
+    Author = models.CharField(max_length=500, default=None, null=True)
+    Citations = models.IntegerField(default=None, null=True)
