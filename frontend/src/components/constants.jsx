165779ebabb5492903980f2b041ce7a71b4e40ed
Yi Kuang Loh <yikuang5@gmail.com>
2022-06-27 23:23:02 +0800

    edit: frontend base url env var change

--- frontend/src/components/constants.jsx
+++ frontend/src/components/constants.jsx
@@ -1,1 +1,1 @@
-export const BASEURL = process.env.REACT_APP_ENVIRONMENT === 'dev' ? process.env.REACT_APP_DEV_BASEURL : process.env.REACT_APP_PRD_BASEURL
+export const BASEURL = process.env.REACT_APP_BASE_URL
